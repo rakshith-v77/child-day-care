c87430ffce89eab141873abd2cc53a5c007dc566
rakshith-v77 <rakshithv622@gmail.com>
2025-11-22 14:56:25 +0530

    Create Daycareapp.java

---
+++ Daycareapp.java
@@ -0,0 +1,12 @@
+package com.example.daycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaycareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaycareApplication.class, args);
+    }
+}
